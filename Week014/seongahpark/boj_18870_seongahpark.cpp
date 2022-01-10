6080ada3372d0b50d3fe674a4f0963c0554925ee
박성아 <sksmsekekek21@naver.com>
2022-01-10 21:28:18 +0900

    Update boj_18870_seongahpark.cpp

--- Week014/seongahpark/boj_18870_seongahpark.cpp
+++ Week014/seongahpark/boj_18870_seongahpark.cpp
@@ -21,7 +21,7 @@ int main() {
 	sort(v2.begin(), v2.end());
 	v2.erase(unique(v2.begin(), v2.end()), v2.end());
 
-	//lower bound로 풀어야 시간초과가 
+	//lower bound로 풀어야 시간초과가 나지 않는다.
 	for (int i = 0; i < n; i++) {
 		cout << lower_bound(v2.begin(), v2.end(), v[i]) - v2.begin() << " ";
 	}
