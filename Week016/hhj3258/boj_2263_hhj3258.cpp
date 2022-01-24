a73c741681a33ae0fa2f95837c1a857e9e3ca689
Hong Hyeon-Jun <70702088+hhj3258@users.noreply.github.com>
2022-01-24 22:50:53 +0900

    현준 16주차 파일명 수정 및 2263 수정

--- Week016/hhj3258/boj_2263_hhj3258.cpp
+++ Week016/hhj3258/boj_2263_hhj3258.cpp
@@ -8,7 +8,7 @@ int arr_inOrder[Size];
 int arr_postOrder[Size];
 unordered_map<int, int> inOrderIdx;
 
-// 전위순회
+// 중위순회와 후위순회를 가지고 전위순회 출력
 void Solve(int inStartIdx, int inEndIdx, int postStartIdx, int postEndIdx)
 {
     if (inStartIdx > inEndIdx)
@@ -20,11 +20,13 @@ void Solve(int inStartIdx, int inEndIdx, int postStartIdx, int postEndIdx)
     int leftTreeSize = rootIdx - inStartIdx;
 
     // 전위순회 : 루트 - 좌 - 우
-    // 루트 출력
-    cout << arr_postOrder[rootIdx] << ' ';
+    // 루트 출력 = 후위 순회의 가장 마지막 인덱스
+    cout << arr_postOrder[postEndIdx] << ' ';
     // 왼쪽 트리 탐색
+    // Solve(그대로, 루트 인덱스 - 1 번째까지, 그대로, 후위순회 시작 인덱스부터 루트 인덱스 이전까지)
     Solve(inStartIdx, rootIdx - 1, postStartIdx, postStartIdx + leftTreeSize - 1);
     // 오른쪽 트리 탐색
+    // Solve(루트 인덱스 + 1 번째부터, 그대로, 루트 인덱스부터, 후위순회 마지막 인덱스(루트 인덱스) 이전까지)
     Solve(rootIdx + 1, inEndIdx, postStartIdx + leftTreeSize, postEndIdx - 1);
 }
 
